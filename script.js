cbf84e64edf2c568599b03eee73e05975c040f20
jballanger <ballanger.julien@gmail.com>
2017-12-24 01:33:42 +0100

    removing bootstrap, export modal, table improvements

---
+++ script.js
@@ -0,0 +1,56 @@
+Vue.component('pos-table', {
+	props: ['mats'],
+	template: '#pos-table-template',
+	filters: {
+		formatIndex: function(val) {
+			return (`${val.charAt(0).toUpperCase()}${val.slice(1)}`);
+		},
+		formatName: function(val) {
+			return (`${val.charAt(0).toUpperCase()}${val.slice(1).replace('_', ' ')}`); 
+		},
+		formatLeft: function(val) {
+			return (val < 0 ? 0 : val);
+		}
+	}
+});
+Vue.component('modal', {
+	props: ['data'],
+	template: '#modal-template',
+	computed: {
+		jsonData: function() {
+			return (JSON.stringify(this.data));
+		}
+	}
+});
+var vm = new Vue({
+	el: '#app',
+	data: {
+		loading: true,
+		data: null,
+		showModal: false
+	},
+	created: function() {
+		this.fetchData();
+	},
+	methods: {
+		merge: function(obj, obj2) {
+			for (var key in obj2) {
+				for (var key2 in obj2[key]) {
+					obj[key][key2].have = obj2[key][key2].have;
+				}
+			}
+			return obj;
+		},
+		fetchData: function() {
+			axios.all([
+				axios.get('data.json'),
+				axios.get('sample.json')
+			])
+			.then(axios.spread((need, sample) => {
+				var data = this.merge(need.data, sample.data);
+				this.loading = false;
+				this.data = data;
+			}));
+		}
+	}
+});
